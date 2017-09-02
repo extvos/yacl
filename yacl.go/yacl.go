9b8dbe225578132f8990f780561ddcdce5d250f3
Mingcai SHEN <archsh@gmail.com>
2017-09-02 14:40:57 +0800

    Updated.

--- yacl.go/yacl.go
+++
@@ -1,3 +0,0 @@
-package yacl
-
-const VERSION = "0.0.1"
---
+++ yaclgo/yacl.go
@@ -0,0 +1,12 @@
+package yaclgo
+
+const VERSION = "0.0.1"
+
+type Parser struct {
+}
+
+// Open ...
+// Open a file as a yacl file, and return a parser if it's ok.
+func Open(filename string, mode string) (*Parser, error) {
+	return nil, nil
+}
